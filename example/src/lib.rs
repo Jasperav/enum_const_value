5806fd01762ad959d1d2edaaaaacaa7bf405fe2b
Jasper <Jasperav@hotmail.com>
2021-08-05 22:52:50 +0200

    Added retrieve ident

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -36,11 +36,13 @@ mod test {
     fn test_my_enum() {
         assert_eq!(0, PlainEnum::SomeCase.const_value());
         assert_eq!(1, PlainEnum::AnotherCase.const_value());
+        assert_eq!("AnotherCase", PlainEnum::AnotherCase.retrieve_ident());
 
         assert_eq!(0, ValueEnum::SomeCase(1).const_value());
         assert_eq!(1, ValueEnum::AnotherCase("MyString".to_string()).const_value());
         assert_eq!(0, ValueEnumConstValue::SomeCase.const_value());
         assert_eq!(1, ValueEnumConstValue::AnotherCase.const_value());
+        assert_eq!("AnotherCase", ValueEnum::AnotherCase("MyString".to_string()).retrieve_ident());
 
         assert_eq!(0, CLikeStructEnum::SomeCase { x: 1 }.const_value());
         assert_eq!(1, CLikeStructEnum::AnotherCase { y: "MyString".to_string() }.const_value());
@@ -50,6 +52,7 @@ mod test {
         assert_eq!(0, MixedEnum::SomeCase.const_value());
         assert_eq!(1, MixedEnum::AnotherCase("MyString".to_string()).const_value());
         assert_eq!(2, MixedEnum::CLikeStruct { x: "MyString".to_string() }.const_value());
+        assert_eq!("CLikeStruct", MixedEnum::CLikeStruct { x: "MyString".to_string() }.retrieve_ident());
         assert_eq!(0, MixedEnumConstValue::SomeCase.const_value());
         assert_eq!(1, MixedEnumConstValue::AnotherCase.const_value());
         assert_eq!(2, MixedEnumConstValue::CLikeStruct.const_value());
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use quote::quote;
-use syn::{DeriveInput, Data, Fields};
+use syn::{DeriveInput, Fields};
 use quote::format_ident;
 
 #[proc_macro_derive(EnumConstValue)]
@@ -8,47 +8,69 @@ pub fn enum_const_value(input: proc_macro::TokenStream) -> proc_macro::TokenStre
     let original_enum_type_name = &derive_input.ident;
     let const_enum_type_ident = format_ident!("{}ConstValue", &original_enum_type_name);
     let mut generate_const_enum = false;
-    let (idents_and_matcher, original_matcher): (Vec<_>, Vec<_>) = match derive_input.data {
-        Data::Enum(e) => {
-            e
-                .variants
-                .into_iter()
-                .enumerate()
-                .map( |(index, variant) | {
-                let index = index as i32;
-                let variant_name = variant.ident;
-                let tokens = match variant.fields {
-                    Fields::Unit => quote! {},
-                    Fields::Unnamed(..) => {
-                        generate_const_enum = true;
-                        quote! {
+    let enum_data = proc_macro2_helper::enum_data(derive_input.data);
+    let retrieve_ident = enum_data
+        .iter()
+        .map(|variant| {
+            let ident = &variant.ident;
+
+            // TODO: not sure how this can be nicer
+            match variant.fields {
+                Fields::Unit => quote! {
+                    Self::#ident => stringify!(#ident)
+                },
+                Fields::Unnamed(..) => quote! {
+                    Self::#ident(_) => stringify!(#ident)
+                },
+                Fields::Named(..) => quote! {
+                    Self::#ident { .. } => stringify!(#ident)
+                }
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let (idents_and_matcher, original_matcher): (Vec<_>, Vec<_>) = enum_data
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|(index, variant)| {
+            let index = index as i32;
+            let variant_name = variant.ident;
+            let tokens = match variant.fields {
+                Fields::Unit => quote! {},
+                Fields::Unnamed(..) => {
+                    generate_const_enum = true;
+                    quote! {
                             (..)
                         }
-                    }
-                    Fields::Named(..) => {
-                        generate_const_enum = true;
-                        quote! {
+                }
+                Fields::Named(..) => {
+                    generate_const_enum = true;
+                    quote! {
                             {..}
                         }
-                    }
-                };
+                }
+            };
 
-                let original_enum_matcher = quote! {
-                        &#original_enum_type_name::#variant_name#tokens => #index
-                    };
-                let const_enum_matcher = quote! {
-                        &#const_enum_type_ident::#variant_name => #index
-                    };
+            let original_enum_matcher = quote! {
+                &#original_enum_type_name::#variant_name#tokens => #index
+            };
+            let const_enum_matcher = quote! {
+                &#const_enum_type_ident::#variant_name => #index
+            };
 
-                ((variant_name, const_enum_matcher), original_enum_matcher)
-            })
-            .unzip()
-        }
-        _ => panic!("Only enums are supported")
-    };
+            ((variant_name, const_enum_matcher), original_enum_matcher)
+        })
+        .unzip();
     let (variant_names, const_matcher): (Vec<_>, Vec<_>) = idents_and_matcher.into_iter().unzip();
     let mut tokens = quote! {
         impl #original_enum_type_name {
+            pub fn retrieve_ident(&self) -> &'static str {
+                match self {
+                    #(#retrieve_ident),*
+                }
+            }
+
             pub fn const_value(&self) -> i32 {
                 match self {
                     #(#original_matcher),*
